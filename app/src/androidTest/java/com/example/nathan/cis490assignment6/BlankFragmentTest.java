fee60429e9fc5af7244ce46d4cc461307486647f
jndoug01 <jndoug01@louisville.edu>
2015-10-26 14:54:03 -0400

    initial commit

---
+++ app/src/androidTest/java/com/example/nathan/cis490assignment6/BlankFragmentTest.java
@@ -0,0 +1,10 @@
+package com.example.nathan.cis490assignment6;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Nathan on 10/26/2015.
+ */
+public class BlankFragmentTest extends TestCase {
+
+}
